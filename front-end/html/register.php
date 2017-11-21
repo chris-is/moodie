a5f591a0a5a12af2df419a0e1ae2bee13abbd255
VU NHU ANH <vunhuanh@gmail.com>
2017-11-20 19:38:13 -0500

    :(

--- front-end/html/register.php
+++ front-end/html/register.php
@@ -9,22 +9,23 @@
   $app->post('/', function ($request, $response) {
     $username = $request->getParam('username');
     $password = $request->getParam('password');
-    $email = $request->getParam('email');
 
-    //Insert username and password into database
-    $insert = "INSERT INTO Users (username, password, email) VALUES ('$username', '$password', '$email');";
-    $dbhost="localhost";
-    $dbuser="root";
-    $dbpass="9pUPOCULo7"; //Password should be changed if set
-    $dbname="Store";
-    $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
-    $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $stmt = $db->query($insert); 
+    $select = "SELECT * FROM Users";
+    try {
+      $db = getDB();
+      $stmt = $db->query($select); 
+      $result = $stmt->fetchAll(PDO::FETCH_OBJ);
+      $db = null;
+      echo $result->username;
+      echo $result->password;
+    } 
 
-    //Call function to print database
-    //$response = printdb($db);
-    //echo $response;
-});
+    //Print error messages if any
+    catch(PDOException $e) {
+      echo json_encode($e->getMessage());
+    }
+    
+  });
 
   
   $app->run();
@@ -37,5 +38,5 @@
   $dbConnection = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass); 
   $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   return $dbConnection;
-}
+  }
 ?>
--- index.php
+++ index.php
@@ -11,21 +11,6 @@
   echo session_id();
   session_start();
 
-  $app->post('/', function ($request, $response) {
-    $username = $request->getParam('username');
-    $password = $request->getParam('password');
-    $email = $request->getParam('email');
-
-    //Insert username and password into database
-    $insert = "INSERT INTO Users (username, password, email) VALUES ('$username', '$password', '$email');";
-    $db = getDB();
-    $stmt = $db->query($insert); 
-
-    //Call function to print database
-    //$response = printdb($db);
-    //echo $response;
-});
-
   header("Location: /COMP307/front-end/html/index.php"); 
   exit();
   $app->run();
