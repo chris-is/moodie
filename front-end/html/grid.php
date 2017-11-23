b372f93bbd53d627f170aedea113e69ef958aa9b
VU NHU ANH <vunhuanh@gmail.com>
2017-11-23 16:22:33 -0500

    ipink alert

--- front-end/html/grid.php
+++ front-end/html/grid.php
@@ -11,6 +11,7 @@
   <button class="grid-btn p" data-toggle="tooltip" data-placement="top"></button>
   <button class="grid-btn p" data-toggle="tooltip" data-placement="top"></button>
   <button id="p-plus" class="plus-ctrl" data-toggle="tooltip" data-placement="top"></button>
+  <button id="alert"></button>
 </div>
 <div class="row" id="g-sad">
   <button id="b-minus" class="minus-ctrl" data-toggle="tooltip" data-placement="top"></button>
@@ -32,57 +33,60 @@
 <script>
   $(document).ready(function() {
     //PINK
-    var pink = $('.p')
     var ipink = 0;
     var interval = null;
     $('#p-plus').on('mousedown', function() {
       interval = setInterval(function() {
-          $("#p-plus").val($("#p-plus").val() + 400);
-          if(ipink < pink.length){
-            $(pink).eq(ipink).addClass('pink');
+          $("#p-plus").val($("#p-plus").val() + 500);
+          if(ipink < 10){
+            $('.p').eq(ipink).addClass('pink');
             ipink++;
           }
-        }, 700);
+        }, 500);
     }).on('mouseup mouseleave', function() {
       clearInterval(interval);
     });
     $('#p-minus').on('mousedown', function() {
       interval = setInterval(function() {
-          $("#p-minus").val($("#p-minus").val() + 400);
-          if(ipink >= 0){
-            $('.p').eq(ipink).removeClass('pink');
+          $("#p-minus").val($("#p-minus").val() + 500);
+          if(ipink > 0){
             ipink--;
+            $('.p').eq(ipink).removeClass('pink');
           }
-        }, 700);
+        }, 500);
     }).on('mouseup mouseleave', function() {
       clearInterval(interval);
     });
 
     //BLUE
-    var blue = $('.b')
     var iblue = 0;
     $('#b-plus').on('mousedown', function() {
       interval = setInterval(function() {
-          $("#b-plus").val($("#b-plus").val() + 400);
-          if(iblue < blue.length){
-            $(blue).eq(iblue).addClass('blue');
+          $("#b-plus").val($("#b-plus").val() + 500);
+          if(iblue < 10){
+            $('.b').eq(iblue).addClass('blue');
             iblue++;
           }
-        }, 700);
+        }, 500);
     }).on('mouseup mouseleave', function() {
       clearInterval(interval);
     });
     $('#b-minus').on('mousedown', function() {
       interval = setInterval(function() {
-          $("#b-minus").val($("#b-minus").val() + 400);
+          $("#b-minus").val($("#b-minus").val() + 500);
           if(iblue >= 0){
-            $('.b').eq(iblue).removeClass('blue');
             iblue--;
+            $('.b').eq(iblue).removeClass('blue');
           }
-        }, 700);
+        }, 500);
     }).on('mouseup mouseleave', function() {
       clearInterval(interval);
     });
+
+    $('#alert').click(function(){
+      alert(ipink);
+    });
+    
       
   });
 
