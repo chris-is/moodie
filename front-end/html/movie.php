2918b32a3fae174a963e4e427b8e834f27569a65
VU NHU ANH <vunhuanh@gmail.com>
2017-11-26 11:19:22 -0500

    movie css done

--- front-end/html/movie.php
+++ front-end/html/movie.php
@@ -46,23 +46,22 @@
 
     <div class="container-fluid">
       <!--SEARCH-->
-
-    <div id="appendSearch"></div>
-      <script>
-        $(function(){
-          $("#appendSearch").load("search.html");
-        });
-      </script>
+      <div id="appendSearch"></div>
+        <script>
+          $(function(){
+            $("#appendSearch").load("search.html");
+          });
+        </script>
 
       <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-5">
           <div class="row">
             <h1 id="name"><?php echo $name?></h1>
             <div id="movieid" style="display: none;">1234</div>
           </div>
           
           <div id="details" class="row">
-            <div id="poster" class="col-sm-5">
+            <div id="poster">
               <img src="https://image.tmdb.org/t/p/w500<?php echo $image_res?>" class="poster-img">
             </div>
           </div>
