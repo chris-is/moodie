e65d3dd0af10a340a02aa2712f2f67d07ba5e3f1
VU NHU ANH <vunhuanh@gmail.com>
2017-12-03 00:24:59 -0500

    simple search 70%
    ai bán cho mình khẩu súng cái
    hay là tí nitrogen cũng đc

--- front-end/html/search-results-simple.php
+++ front-end/html/search-results-simple.php
@@ -49,6 +49,33 @@
       
 
 
+      <script>
+      $(document).ready(function(){
+        var query = window.location.search.substring(1);
+        var url = "https://api.themoviedb.org/3/search/movie?api_key=1753a8a0eee9f02ab07f902370f8f1ea&language=en-US&query=" + query + "&page=1&include_adult=false";
+
+        var settings = {
+          "async": true,
+          "crossDomain": true,
+          "url": url,
+          "method": "GET",
+          "headers": {},
+          "data": "{}"
+        }
+
+        $.ajax(settings).done(function (response) {
+          for(var i=0; i<1; i++){
+            var poster = "https://image.tmdb.org/t/p/w500" + response.results[i].poster_path;
+            $('#poster0').attr('src', poster);
+            console.log(poster);
+          }
+
+        });
+
+
+      });
+      </script>
+
 
     </div>
 
